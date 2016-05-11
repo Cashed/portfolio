415c1f41aa53c9fe7bc0723171b76e6aa63d4259
Veronica Moreno <frau.veronika@gmail.com>
2016-05-11 12:18:23 -0700

    fix to row pushing out of parent div, parallax header

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $('header').parallax({
+    imageSrc: "images/masthead-background1.jpg",
+    naturalHeight: 2848,
+    naturalWidth: 4288,
+    speed: 0.6
+    }
+  );
+
+});
