0dfb589c751ebe9c5785cd4cf9de37441e53c128
Veronica Moreno <frau.veronika@gmail.com>
2016-08-05 14:45:58 -0700

    resume modal and styling

--- js/script.js
+++ js/script.js
@@ -1,41 +1,49 @@
-$(document).ready(function() {
-  $('.main-header').parallax({
-    imageSrc: "images/masthead-background1.jpg",
-    naturalHeight: 2848,
-    naturalWidth: 4288,
-    speed: 0.5
-    }
-  );
+(function() {
+  $(document).ready(function() {
 
-  $('.unity-header').parallax({
-    imageSrc: "images/unity/unity.jpg",
-    naturalHeight: 422,
-    naturalWidth: 750,
-    speed: 0.5
-    }
-  );
+    // parallax header
+    $('.main-header').parallax({
+      imageSrc: "images/masthead-background1.jpg",
+      naturalHeight: 2848,
+      naturalWidth: 4288,
+      speed: 0.5
+    });
 
-  $(window).scroll(function() {
-    var scroll = $(window).scrollTop();
+    $('.unity-header').parallax({
+      imageSrc: "images/unity/unity.jpg",
+      naturalHeight: 422,
+      naturalWidth: 750,
+      speed: 0.5
+    });
 
-    if (scroll >= 600) {
-      $('.navbar').addClass('solidBackground');
-    }
-    else {
-      $('.navbar').removeClass('solidBackground');
-    }
-  });
+    // menu scroll effect
+    $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
 
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 800);
-        return false;
+      if (scroll >= 600) {
+        $('.navbar').addClass('solidBackground');
       }
-    }
+      else {
+        $('.navbar').removeClass('solidBackground');
+      }
+    });
+
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 800);
+          return false;
+        }
+      }
+    });
+
+    // pdf resume popup
+    $('.resume-popup').click(function() {
+      $('#resume').fadeIn();
+    });
   });
-});
+})();
